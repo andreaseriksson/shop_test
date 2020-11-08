c7238665754f0fa947c01b51e9b998e027958c6a
Andreas Eriksson <andreas@codered.se>
2020-11-08 18:53:45 +0100

    Add toggle all, bulk update and bulk delete to products

--- lib/shop_test_web/live/product_live/index.ex
+++ lib/shop_test_web/live/product_live/index.ex
@@ -3,6 +3,7 @@ defmodule ShopTestWeb.ProductLive.Index do
 
   import Ecto.Changeset
   alias ShopTest.Products
+  alias ShopTest.Products.Product
 
   @impl true
   def mount(_params, _session, socket) do
@@ -20,6 +21,8 @@ defmodule ShopTestWeb.ProductLive.Index do
       socket
       |> assign(:order_and_filter_changeset, order_and_filter_changeset(params))
       |> assign(:products, list_products(params))
+      |> assign(:toggle_ids, [])
+      |> assign(:price_changeset, price_changeset())
     }
   end
 
@@ -55,6 +58,83 @@ defmodule ShopTestWeb.ProductLive.Index do
     end
   end
 
+  def handle_event("toggle-all", %{"value" => "on"}, socket) do
+    product_ids = socket.assigns.products |> Enum.map(& &1.id)
+    {:noreply, assign(socket, :toggle_ids, product_ids)}
+  end
+
+  def handle_event("toggle-all", %{}, socket) do
+    {:noreply, assign(socket, :toggle_ids, [])}
+  end
+
+  def handle_event("toggle", %{"toggle-id" => id}, socket) do
+    id = String.to_integer(id)
+    toggle_ids = socket.assigns.toggle_ids
+
+    toggle_ids =
+      if (id in toggle_ids) do
+        Enum.reject(toggle_ids, & &1 == id)
+      else
+        [id|toggle_ids]
+      end
+
+    {:noreply, assign(socket, :toggle_ids, toggle_ids)}
+  end
+
+  def handle_event("delete", %{}, socket) do
+    toggle_ids = socket.assigns.toggle_ids
+    products = Enum.reject(socket.assigns.products, & &1.id in toggle_ids)
+    products_to_be_deleted = Enum.filter(socket.assigns.products, & &1.id in toggle_ids)
+
+    Task.async(fn ->
+      products_to_be_deleted
+      |> Enum.each(& Products.delete_product/1)
+    end)
+
+    {
+      :noreply,
+      socket
+      |> assign(:toggle_ids, [])
+      |> assign(:products, products)
+    }
+  end
+
+  def handle_event("bulk-action", %{"product" => params}, socket) do
+    {:noreply,  assign(socket, :price_changeset, price_changeset(params))}
+  end
+
+  def handle_event("bulk-trigger", _, socket) do
+    changeset = socket.assigns.price_changeset
+    toggle_ids = socket.assigns.toggle_ids
+
+    products =
+      if changeset.valid? do
+        socket.assigns.products
+        |> Enum.map(fn product ->
+          # Check if the product should update
+          if product.id in toggle_ids do
+            {:ok, product} = Products.update_product(product, changeset.changes)
+            product
+          else
+            product
+          end
+        end)
+      else
+        socket.assigns.products
+      end
+
+    {
+      :noreply,
+      socket
+      |> assign(:price_changeset, price_changeset())
+      |> assign(:products, products)
+      |> assign(:toggle_ids, [])
+    }
+  end
+
+  @impl true
+  def handle_info(_, socket), do: {:noreply, socket}
+
   defp list_products(params) do
     Products.list_products(params)
   end
@@ -73,11 +153,20 @@ defmodule ShopTestWeb.ProductLive.Index do
     |> validate_format(:search_phrase, ~r/[A-Za-z0-9\ ]/)
   end
 
-  defp order_and_filter_changeset(attrs \\ %{}) do
+  defp order_and_filter_changeset(attrs) do
     cast(
       {%{}, %{order_by: :string}},
       attrs,
       [:order_by]
     )
+  end
+
+  defp price_changeset(attrs \\ %{}) do
+    cast(
+      {%{}, %{price: :decimal}},
+      attrs,
+      [:price]
+    )
+    |> validate_required([:price])
   end
 end
